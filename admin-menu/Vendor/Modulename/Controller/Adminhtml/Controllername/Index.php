8a90a565ce22616ea85356f7df11548e575b9094
kapil <kapildsingh89@gmail.com>
2018-08-30 23:26:06 +0530

    admin menu and page module

---
+++ admin-menu/Vendor/Modulename/Controller/Adminhtml/Controllername/Index.php
@@ -0,0 +1,42 @@
+<?php
+/** 
+ * @package   Vendor_Modulename
+ * @author    Ricky Thakur (Kapil Dev Singh)
+ * @copyright Copyright (c) 2018 Ricky Thakur
+ * @license   community edition, reusable and editable
+ */
+    namespace Vendor\Modulename\Controller\Adminhtml\Controllername;
+
+    class Index extends \Magento\Backend\App\Action
+    {
+    /**
+    * @var \Magento\Framework\View\Result\PageFactory
+    */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+            parent::__construct($context);
+            $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/route_id_controllername_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+            return  $resultPage = $this->resultPageFactory->create();
+    }
+    }
+?>
+  
---
+++ admin-menu/Vendor/Modulename/registration.php
@@ -0,0 +1,12 @@
+<?php
+/** 
+ * @package   Vendor_Modulename
+ * @author    Ricky Thakur (Kapil Dev Singh)
+ * @copyright Copyright (c) 2018 Ricky Thakur
+ * @license   community edition, reusable and editable
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vendor_Modulename',
+    __DIR__
+);
