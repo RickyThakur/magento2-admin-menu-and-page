e19c2b0f73a13551a14e2ca20862f4305b6cdb52
kapil <kapildsingh89@gmail.com>
2018-09-09 19:34:08 +0530

    upgrade license

--- admin-menu/Vendor/Modulename/registration.php
+++ admin-menu/Vendor/Modulename/registration.php
@@ -3,7 +3,7 @@
  * @package   Vendor_Modulename
  * @author    Ricky Thakur (Kapil Dev Singh)
  * @copyright Copyright (c) 2018 Ricky Thakur
- * @license   community edition, reusable and editable
+ * @license   MIT license (see LICENSE.txt for details
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
